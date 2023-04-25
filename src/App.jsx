333666b64c8bcb53c428db3213c4b472d50f1a93
Marius CA <mariuskcodeacademy@gmail.com>
2023-04-25 15:00:31 +0300

    init tailwind

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className='container mx-auto max-w-5xl '>
+      <h1 className='lg:text-5xl md:text-4xl sm:text-2xl text-xl text-center mt-10 underline'>
+        Hello world!
+      </h1>
+      <h1>As esu h1</h1>
+      <h2>As esu h2</h2>
+    </div>
+  );
+}
+
+export default App;
